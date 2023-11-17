f247b10961660f71fe2bd1780b12f3d50e0866cc
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-11-17 08:44:35 -0300

    structuring project in classes format

--- chat/client/client.py
+++ chat/client/client.py
@@ -1,4 +1,38 @@
 import socket
 import client.gui as gui
+from typing import Callable, Self
 
+
+def broadcast(conns: list[socket.socket] = []):
+    def broadcast_operation(operation: Callable):
+        def wrapper(*args, **kwargs):
+            for conn in conns:
+                operation(conn, *args, **kwargs)
+        return wrapper
+    return broadcast_operation
+
+
+class Client:
+    def __new__(cls) -> Self:
+        pass
+
+    def __init__(self) -> None:
+        pass
+
+    def boot_client(self) -> None:
+        pass
+
+    def start(self) -> None:
+        pass
+
+    def send(self, data: bytes) -> None:
+        pass
+
+    def receive(self) -> bytes:
+        pass
+
+    def handle_msg(self, data: bytes) -> None:
+        pass
+
+    
 def main(host: str, port: int): ...
--- chat/server/server.py
+++ chat/server/server.py
@@ -1,4 +1,42 @@
 import socket
+from typing import Callable, Self
 
 
-def main(host: str, port: int): ...
+
+def broadcast(conns: list[socket.socket] = []): 
+    def broadcast_operation(operation: Callable):
+        def wrapper(*args, **kwargs):
+            for conn in conns:
+                operation(conn, *args, **kwargs)
+        return wrapper
+    return broadcast_operation 
+
+class Server:
+    conns: list[socket.socket]
+
+    __instance: Self | None = None
+
+    def __new__(cls) -> Self:
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.conns = []
+        return cls.__instance
+
+    def __init__(self) -> None: 
+        pass
+
+    def boot_server(self): ...
+    def start(self): ...
+    def handle_client(self, client_socket: socket.socket, addr: tuple): ...
+    def send(self, conn: socket.socket, data: bytes): ...
+    def receive(self, conn: socket.socket) -> bytes: ...
+    def handle_msg(self, conn: socket.socket, data: bytes): ...
+
+
+def main(host: str, port: int):
+    server = Server()
+    server.boot_server()
+    server.start()
+    
+if __name__ == '__main__':
+    main()
